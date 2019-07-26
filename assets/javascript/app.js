91be41bd3f6f10e23c411b96c1e93ce1cf4334e2
Maria Shady <maria.shady@yahoo.com>
2019-07-26 15:17:18 -0400

    set up functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    //Start game when user clicks start button
+    $("#startButton").on("click", gameState.startTimer);
+
+});
+
+let gameState = {
+    
+    timeRemaining: 60,
+
+    startTimer: function() {
+        $("#timer").text("Time remaining: " + gameState.timeRemaining);
+        setInterval(gameState.countdown, 1000);
+        $("#startPage").hide();
+        trivia.displayQuestions();
+
+    },
+        
+    countdown: function() {
+        gameState.timeRemaining--;
+        $("#timer").text("Time remaining: " + gameState.timeRemaining);
+        if (gameState.timeRemaining === 0) {
+        gameState.stopTimer();
+        $("#timer").empty();
+        }
+    },
+
+    stopTimer: function() {
+        clearInterval();
+        trivia.checkAnswers();
+    },
+
+    showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
+        $("endPage").show();
+        $("questionsBox").empty();
+        $("#timer").empty();
+        $("#timer").hide();
+        $("#correctAnswers").text("Correct Answers: " + numCorrect);
+        $("#incorrectAnswers").text("Incorrect Answers: " + numIncorrect);
+        $("#unanswered").text("Skipped Questions: " + numUnanswered);
+    }
+}
+
+let trivia = {
+    
+}
+
+
+};
+console.log(gameState);
+
+
+
+
+
     
 
 
@@ -9,16 +61,3 @@ $(document).ready(function(){
 
 
 
-//Timer code
-let seconds = 30;
-function countdown() {
-  seconds--;
-  console.log(seconds);
-  $(".countdownArea").text(`Time Left: ${seconds}`);
-  if (seconds === 0) {
-    clearTimeout(timer);
-    return false;
-  }
-  setTimeout(countdown, 1000)
-}
-const timer = setTimeout(countdown, 1000);
